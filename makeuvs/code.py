20f742955ea7033fbe02c9664e7f45e04050d9eb
ELiZSwe <mikael.jc.soderberg@gmail.com>
2025-06-20 21:35:55 +0200

    Whitespace

--- makeuvs/code.py
+++ makeuvs/code.py
@@ -31,6 +31,8 @@ def mesh_uv_wrap(mesh, maxIterations):
     #chart_options.textureSeamWeight = 0.5
     #chart_options.maxCost = 2.0
     chart_options.maxIterations = maxIterations  #org = 1
+    
+    
     #chart_options.useInputMeshUvs =  False
     #chart_options.fixWinding = False
     
