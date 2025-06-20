78d6b3b14cf72174c6f5bd534067708507715da0
ELiZSwe <mikael.jc.soderberg@gmail.com>
2025-06-20 20:07:47 +0200

    Renaming options

--- makeuvs/code.py
+++ makeuvs/code.py
@@ -10,28 +10,28 @@ def mesh_uv_wrap(mesh):
     myAtlas = xatlas.Atlas()
     myAtlas.add_mesh(mesh.vertices, mesh.faces)
 
-    packOptions = PackOptions()
-    #packOptions.maxChartSize = 4096
-    #packOptions.padding = 1
-    #packOptions.resolution = 4096
-    #packOptions.blockAlign = True
-    #packOptions.bruteForce = True
-    packOptions.create_image = True
+    pack_options = xatlas.PackOptions()
+    pack_options.create_image = True
+    #pack_options.maxChartSize = 4096
+    #pack_options.padding = 1
+    #pack_options.resolution = 4096
+    #pack_options.blockAlign = True
+    #pack_options.bruteForce = True
 
-    chartOptions = ChartOptions()
-    #chartOptions.maxChartArea = 0.0
-    #chartOptions.maxBoundaryLength = 0.0
-    #chartOptions.normalDeviationWeight = 2.0
-    #chartOptions.roundnessWeight = 0.01
-    #chartOptions.straightnessWeight = 6.0
-    #chartOptions.normalSeamWeight = 4.0
-    #chartOptions.textureSeamWeight = 0.5
-    #chartOptions.maxCost = 2.0
-    #chartOptions.maxIterations = 2  #org = 1
-    #chartOptions.useInputMeshUvs =  False
-    #chartOptions.fixWinding = False
+    chart_options = xatlas.ChartOptions()
+    #chart_options.maxChartArea = 0.0
+    #chart_options.maxBoundaryLength = 0.0
+    #chart_options.normalDeviationWeight = 2.0
+    #chart_options.roundnessWeight = 0.01
+    #chart_options.straightnessWeight = 6.0
+    #chart_options.normalSeamWeight = 4.0
+    #chart_options.textureSeamWeight = 0.5
+    #chart_options.maxCost = 2.0
+    #chart_options.maxIterations = 2  #org = 1
+    #chart_options.useInputMeshUvs =  False
+    #chart_options.fixWinding = False
 
-    myAtlas.generate(pack_options=packOptions, chart_options = chartOptions, )
+    myAtlas.generate(pack_options=pack_options, chart_options = chart_options)
 
     image = myAtlas.chart_image
 
