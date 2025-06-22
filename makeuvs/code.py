fa0c73f6ba9c637cb3cbfcab2c7e606ba0919228
ELiZSwe <mikael.jc.soderberg@gmail.com>
2025-06-22 02:58:00 +0200

    Added the last of the properties

--- makeuvs/code.py
+++ makeuvs/code.py
@@ -7,95 +7,62 @@ from PIL import Image
 import numpy as np
 from xatlas import PackOptions, ChartOptions
 
-def mesh_uv_wrap(mesh, max_chart_area, max_cost, maxIterations, resolution, blockAlign, fix_winding, rotate_charts, rotate_charts_to_axis, padding, bruteForce):
+def mesh_uv_wrap(mesh, max_chart_area, max_cost, maxIterations, resolution, blockAlign, fix_winding, rotate_charts, rotate_charts_to_axis, padding, bruteForce, max_boundary_length, normal_deviation_weight, roundness_weight, straightness_weight, normal_seam_weight, texture_seam_weight, max_chart_size, texels_per_unit, bilinear, create_image, use_input_mesh_uvs):
     if isinstance(mesh, trimesh.Scene):
         mesh = mesh.dump(concatenate=True)
 
     myAtlas = xatlas.Atlas()
     myAtlas.add_mesh(mesh.vertices, mesh.faces)
     
-    #These Can be used
-    #    .def_readwrite("max_chart_size", &xatlas::PackOptions::maxChartSize, "Charts larger than this will be scaled down. 0 means no limit.")
-    #    .def_readwrite("texels_per_unit", &xatlas::PackOptions::texelsPerUnit, R"doc(Unit to texel scale. e.g. a 1x1 quad with texelsPerUnit of 32 will take up approximately 32x32 texels in the atlas.
-    #If 0, an estimated value will be calculated to approximately match the given resolution.
-    #If resolution is also 0, the estimated value will approximately match a 1024x1024 atlas.
-	#    )doc")
-    #    .def_readwrite("resolution", &xatlas::PackOptions::resolution, R"doc(If 0, generate a single atlas with texelsPerUnit determining the final resolution. 
-    #If not 0, and texelsPerUnit is not 0, generate one or more atlases with that exact resolution. 
-    #If not 0, and texelsPerUnit is 0, texelsPerUnit is estimated to approximately match the resolution.)doc")
-    #    .def_readwrite("bilinear", &xatlas::PackOptions::bilinear, "Leave space around charts for texels that would be sampled by bilinear filtering.")
-    #    .def_readwrite("bruteForce", &xatlas::PackOptions::bruteForce, "Slower, but gives the best result. If false, use random chart placement.")
-    #    .def_readwrite("create_image", &xatlas::PackOptions::createImage, "Create Atlas::image.")
-
+    #Using the names from here:
+    #https://github.com/mworchel/xatlas-python/blob/main/src/options.cpp
     pack_options = xatlas.PackOptions()
-    pack_options.create_image = True
-    #pack_options.max_chart_size = 4096
-    pack_options.resolution = resolution
-    pack_options.blockAlign = blockAlign
-    pack_options.rotate_charts = rotate_charts
+    pack_options.create_image          = create_image
+    pack_options.max_chart_size        = max_chart_size
+    pack_options.padding               = padding
+    pack_options.texels_per_unit       = texels_per_unit
+    pack_options.resolution            = resolution
+    pack_options.bilinear              = bilinear
+    pack_options.blockAlign            = blockAlign
+    pack_options.rotate_charts         = rotate_charts
     pack_options.rotate_charts_to_axis = rotate_charts_to_axis
-    pack_options.padding = padding
-    pack_options.bruteForce = bruteForce
+    pack_options.bruteForce            = bruteForce
 
     chart_options = xatlas.ChartOptions()
-
-        #These can be used
-        #.def_readwrite("max_boundary_length", &xatlas::ChartOptions::maxBoundaryLength)
-        #.def_readwrite("normal_deviation_weight", &xatlas::ChartOptions::normalDeviationWeight)
-        #.def_readwrite("roundness_weight", &xatlas::ChartOptions::roundnessWeight)
-        #.def_readwrite("straightness_weight", &xatlas::ChartOptions::straightnessWeight)
-        #.def_readwrite("normal_seam_weight", &xatlas::ChartOptions::normalSeamWeight)
-        #.def_readwrite("texture_seam_weight", &xatlas::ChartOptions::textureSeamWeight)
-        #.def_readwrite("max_cost", &xatlas::ChartOptions::maxCost)
-        #.def_readwrite("use_input_mesh_uvs", &xatlas::ChartOptions::useInputMeshUvs)
-
-
-    #chart_options.maxChartArea = 0.0
-    #chart_options.maxBoundaryLength = 0.0
-    #chart_options.normalDeviationWeight = 2.0
-    #chart_options.roundnessWeight = 0.01
-    #chart_options.straightnessWeight = 6.0
-    #chart_options.normalSeamWeight = 4.0
-    #chart_options.textureSeamWeight = 0.5
-    #chart_options.maxCost = 1.0
     chart_options.max_chart_area = max_chart_area
     chart_options.max_cost = max_cost
     chart_options.max_iterations = maxIterations
     chart_options.fix_winding = fix_winding
-    #chart_options.straightness_weight = 4.0
-    #chart_options.useInputMeshUvs =  False
-    
+    chart_options.max_boundary_length = max_boundary_length
+    chart_options.normal_deviation_weight = normal_deviation_weight
+    chart_options.roundness_weight = roundness_weight
+    chart_options.straightness_weight = straightness_weight
+    chart_options.normal_seam_weight = normal_seam_weight
+    chart_options.texture_seam_weight = texture_seam_weight
+    chart_options.use_input_mesh_uvs = use_input_mesh_uvs
+
     myAtlas.generate(pack_options=pack_options, chart_options = chart_options)
-    
-    #print ("maxIterations: "  + str(maxIterations))
-    #print ("max_chart_area: " + str(max_chart_area))
-    #print ("max_cost: "       + str(max_cost))
-    #print ("Width: "          + str(myAtlas.width))
-    #print ("Height: "         + str(myAtlas.height))
-    #print ("Utilization: "    + str(myAtlas.utilization))
-    #print ("atlasCount: "     + str(myAtlas.atlas_count))
-    #print ("chartCount: "     + str(myAtlas.chart_count))
-   
-    #Create and nparray from the Chart
-    np_myimage = myAtlas.get_chart_image(0)
-    UVImage = Image.fromarray(np.uint8(np_myimage))
-    UVImage = UVImage.transpose(Image.FLIP_TOP_BOTTOM)
+    if image_transformed == True:
+        #Create and nparray from the Chart
+        np_myimage = myAtlas.get_chart_image(0)
+        UVImage = Image.fromarray(np.uint8(np_myimage))
+        UVImage = UVImage.transpose(Image.FLIP_TOP_BOTTOM)
+        if resolution < 1:
+            #From this point on, this is only used for Image output
+            resolution = 1024
 
-    if resolution < 1:
-        #From this point on, this is only used for Image output
-        resolution = 1024
-
-    transform_pipeline = transforms.Compose([
-        transforms.Resize((resolution, resolution)),
-        transforms.ToTensor(),
-    ])
-    image_transformed = transform_pipeline(UVImage)
-    image_transformed = image_transformed.permute(1, 2, 0).unsqueeze(0)
+        transform_pipeline = transforms.Compose([
+            transforms.Resize((resolution, resolution)),
+            transforms.ToTensor(),
+        ])
+        image_transformed = transform_pipeline(UVImage)
+        image_transformed = image_transformed.permute(1, 2, 0).unsqueeze(0)
+    else:
+        image_transformed = None
 
     #the built in code4
     vmapping, indices, uvs = myAtlas[0]
-    #vmapping, indices, uvs = xatlas.parametrize(mesh.vertices, mesh.faces)
-
+    
     mesh.vertices = mesh.vertices[vmapping]
     mesh.faces = indices
     mesh.visual.uv = uvs
--- nodes.py
+++ nodes.py
@@ -15,16 +15,28 @@ class ELiZMeshUVWrap:
                 "trimesh": ("TRIMESH",),
             },
             "optional": {
-                "max_chart_area": ("FLOAT", {"default": 0.0, "min": 0.0, "max": 8.0, "tooltip": "Don\'t grow charts to be larger than this. 0 means no limit."}),
-                "max_cost": ("FLOAT", {"default": 2.0, "min": 2.0, "max": 24.0, "tooltip": "If total of all metrics * weights > maxCost, don't grow chart. Lower values result in more charts."}),
-                "maxIterations": ("INT", {"default": 1, "min": 1, "max": 32, "tooltip": "Number of iterations of the chart growing and seeding phases. Higher values result in better charts."}),
-                "resolution": ("INT", {"default": 0, "min": 0, "max": 4096, "tooltip": "If 0, generate a single atlas with texelsPerUnit determining the final resolution. If not 0, and texelsPerUnit is not 0, generate one or more atlases with that exact resolution. If not 0, and texelsPerUnit is 0, texelsPerUnit is estimated to approximately match the resolution."}),
-                "blockAlign": ("BOOLEAN", {"default": False, "tooltip": "Align charts to 4x4 blocks. Also improves packing speed, since there are fewer possible chart locations to consider."}),
-                "fix_winding": ("BOOLEAN", {"default": False, "tooltip": "Enforce consistent texture coordinate winding."}),
-                "rotate_charts": ("BOOLEAN", {"default": True, "tooltip": "Rotate charts to improve packing."}),
-                "rotate_charts_to_axis": ("BOOLEAN", {"default": True, "tooltip": "Rotate charts to the axis of their convex hull."}),
-                "padding": ("INT", {"default": 0, "min": 0, "max": 32, "tooltip": "Number of pixels to pad charts with"}),
-                "bruteForce": ("BOOLEAN", {"default": False, "tooltip": "Slower, but gives the best result. If false, use random chart placement."}),
+                "max_chart_area":          ("FLOAT", {"default": 0.0, "min": 0.0, "max": 8.0,   "tooltip": "Chart Option: Don\'t grow charts to be larger than this. 0 means no limit."}),
+                "max_boundary_length":     ("FLOAT", {"default": 0.0, "min": 0.0, "max": 8.0,   "tooltip": "Chart Option: Don't grow charts to have a longer boundary than this. 0 means no limit."}),
+                "normal_deviation_weight": ("FLOAT", {"default": 2.0, "min": 0.0, "max": 8.0,   "tooltip": "Chart Option: Angle between face and average chart normal."}),
+                "roundness_weight":        ("FLOAT", {"default": 0.01, "min": 0.0, "max": 8.0,  "tooltip": "Chart Option: "}),
+                "straightness_weight":     ("FLOAT", {"default": 6.0, "min": 0.0, "max": 8.0,   "tooltip": "Chart Option: "}),
+                "normal_seam_weight":      ("FLOAT", {"default": 4.0, "min": 0.0, "max": 8.0,   "tooltip": "Chart Option: If > 1000, normal seams are fully respected."}),
+                "texture_seam_weight":     ("FLOAT", {"default": 0.5, "min": 0.0, "max": 8.0,   "tooltip": "Chart Option: "}),
+                "max_cost":                ("FLOAT", {"default": 2.0, "min": 2.0, "max": 24.0,  "tooltip": "Chart Option: If total of all metrics * weights > maxCost, don't grow chart. Lower values result in more charts."}),
+                "maxIterations":           ("INT", {"default": 1, "min": 1, "max": 32,          "tooltip": "Chart Option: Number of iterations of the chart growing and seeding phases. Higher values result in better charts."}),
+                "use_input_mesh_uvs":      ("BOOLEAN", {"default": False,                       "tooltip": "Chart Option: "}),
+                "fix_winding":             ("BOOLEAN", {"default": False,                       "tooltip": "Chart Option: Enforce consistent texture coordinate winding."}),
+
+                "max_chart_size":          ("INT", {"default": 0, "min": 0, "max": 100,         "tooltip": "Pack Option: Charts larger than this will be scaled down. 0 means no limit."}),
+                "padding":                 ("INT", {"default": 0, "min": 0, "max": 100,         "tooltip": "Pack Option: Number of pixels to pad charts with"}),
+                "texels_per_unit":         ("FLOAT", {"default": 0.0, "min": 0.0, "max": 8.0,   "tooltip": "Pack Option: \n// Unit to texel scale. e.g. a 1x1 quad with texelsPerUnit of 32 will take up approximately 32x32 texels in the atlas.\n// If 0, an estimated value will be calculated to approximately match the given resolution.\n// If resolution is also 0, the estimated value will approximately match a 1024x1024 atlas."}),
+                "resolution":              ("INT", {"default": 0, "min": 0, "max": 4096,        "tooltip": "Pack Option: If 0, generate a single atlas with texelsPerUnit determining the final resolution. If not 0, and texelsPerUnit is not 0, generate one or more atlases with that exact resolution. If not 0, and texelsPerUnit is 0, texelsPerUnit is estimated to approximately match the resolution."}),
+                "bilinear":                ("BOOLEAN", {"default": True,                        "tooltip": "Pack Option: Leave space around charts for texels that would be sampled by bilinear filtering."}),
+                "blockAlign":              ("BOOLEAN", {"default": False,                       "tooltip": "Pack Option: Align charts to 4x4 blocks. Also improves packing speed, since there are fewer possible chart locations to consider."}),
+                "bruteForce":              ("BOOLEAN", {"default": False,                       "tooltip": "Pack Option: Slower, but gives the best result. If false, use random chart placement."}),
+                "create_image":            ("BOOLEAN", {"default": False,                       "tooltip": "Pack Option: Create Atlas::image"}),
+                "rotate_charts":           ("BOOLEAN", {"default": True,                        "tooltip": "Pack Option: Rotate charts to improve packing."}),
+                "rotate_charts_to_axis":   ("BOOLEAN", {"default": True,                        "tooltip": "Pack Option: Rotate charts to the axis of their convex hull."}),
             }            
         }
 
@@ -33,9 +45,9 @@ class ELiZMeshUVWrap:
     FUNCTION = "process"
     CATEGORY = "ELiZTools"
 
-    def process(self, trimesh, max_chart_area, max_cost, maxIterations, resolution, blockAlign, fix_winding, rotate_charts, rotate_charts_to_axis, padding, bruteForce):
+    def process(self, trimesh, max_chart_area, max_cost, maxIterations, resolution, blockAlign, fix_winding, rotate_charts, rotate_charts_to_axis, padding, bruteForce, max_boundary_length, normal_deviation_weight, roundness_weight, straightness_weight, normal_seam_weight, texture_seam_weight, max_chart_size, texels_per_unit, bilinear, create_image, use_input_mesh_uvs):
         from .makeuvs.code import mesh_uv_wrap
-        trimesh, UVImage = mesh_uv_wrap(trimesh, max_chart_area, max_cost, maxIterations, resolution, blockAlign, fix_winding, rotate_charts, rotate_charts_to_axis, padding, bruteForce)
+        trimesh, UVImage = mesh_uv_wrap(trimesh, max_chart_area, max_cost, maxIterations, resolution, blockAlign, fix_winding, rotate_charts, rotate_charts_to_axis, padding, bruteForce, max_boundary_length, normal_deviation_weight, roundness_weight, straightness_weight, normal_seam_weight, texture_seam_weight, max_chart_size, texels_per_unit, bilinear, create_image, use_input_mesh_uvs)
         return (trimesh, UVImage)
 
 NODE_CLASS_MAPPINGS = {
