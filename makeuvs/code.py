df0ed3d9caefdc20b4978ab827c3a3911e46ff02
ELiZSwe <mikael.jc.soderberg@gmail.com>
2025-06-22 03:02:28 +0200

    Fixed formatting

--- makeuvs/code.py
+++ makeuvs/code.py
@@ -13,36 +13,37 @@ def mesh_uv_wrap(mesh, max_chart_area, max_cost, maxIterations, resolution, bloc
 
     myAtlas = xatlas.Atlas()
     myAtlas.add_mesh(mesh.vertices, mesh.faces)
-    
+
     #Using the names from here:
     #https://github.com/mworchel/xatlas-python/blob/main/src/options.cpp
     pack_options = xatlas.PackOptions()
-    pack_options.create_image          = create_image
-    pack_options.max_chart_size        = max_chart_size
-    pack_options.padding               = padding
-    pack_options.texels_per_unit       = texels_per_unit
-    pack_options.resolution            = resolution
-    pack_options.bilinear              = bilinear
-    pack_options.blockAlign            = blockAlign
-    pack_options.rotate_charts         = rotate_charts
-    pack_options.rotate_charts_to_axis = rotate_charts_to_axis
-    pack_options.bruteForce            = bruteForce
+    pack_options.create_image             = create_image
+    pack_options.max_chart_size           = max_chart_size
+    pack_options.padding                  = padding
+    pack_options.texels_per_unit          = texels_per_unit
+    pack_options.resolution               = resolution
+    pack_options.bilinear                 = bilinear
+    pack_options.blockAlign               = blockAlign
+    pack_options.rotate_charts            = rotate_charts
+    pack_options.rotate_charts_to_axis    = rotate_charts_to_axis
+    pack_options.bruteForce               = bruteForce
 
     chart_options = xatlas.ChartOptions()
-    chart_options.max_chart_area = max_chart_area
-    chart_options.max_cost = max_cost
-    chart_options.max_iterations = maxIterations
-    chart_options.fix_winding = fix_winding
-    chart_options.max_boundary_length = max_boundary_length
+    chart_options.max_chart_area          = max_chart_area
+    chart_options.max_cost                = max_cost
+    chart_options.max_iterations          = maxIterations
+    chart_options.fix_winding             = fix_winding
+    chart_options.max_boundary_length     = max_boundary_length
     chart_options.normal_deviation_weight = normal_deviation_weight
-    chart_options.roundness_weight = roundness_weight
-    chart_options.straightness_weight = straightness_weight
-    chart_options.normal_seam_weight = normal_seam_weight
-    chart_options.texture_seam_weight = texture_seam_weight
-    chart_options.use_input_mesh_uvs = use_input_mesh_uvs
+    chart_options.roundness_weight        = roundness_weight
+    chart_options.straightness_weight     = straightness_weight
+    chart_options.normal_seam_weight      = normal_seam_weight
+    chart_options.texture_seam_weight     = texture_seam_weight
+    chart_options.use_input_mesh_uvs      = use_input_mesh_uvs
 
     myAtlas.generate(pack_options=pack_options, chart_options = chart_options)
-    if image_transformed == True:
+    
+    if create_image == True:
         #Create and nparray from the Chart
         np_myimage = myAtlas.get_chart_image(0)
         UVImage = Image.fromarray(np.uint8(np_myimage))
@@ -58,7 +59,7 @@ def mesh_uv_wrap(mesh, max_chart_area, max_cost, maxIterations, resolution, bloc
         image_transformed = transform_pipeline(UVImage)
         image_transformed = image_transformed.permute(1, 2, 0).unsqueeze(0)
     else:
-        image_transformed = None
+        image_transformed = None    
 
     #the built in code4
     vmapping, indices, uvs = myAtlas[0]
